150b552f44d9da3fb3a06047ae787d5715c571ca
small-navigate <1766730733@qq.com>
2020-05-13 21:26:21 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import Add from '../components/goods/Add.vue'
+import Report from '../components/report/Report.vue'
 // eslint-disable-next-line camelcase
 import GoodsList from '../components/goods/Goods_list.vue'
 import Order from '../components/order/Order.vue'
@@ -30,7 +31,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: GoodsList },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   }
 ]
